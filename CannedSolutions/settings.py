f3aebdd9832446fdd7142508ce10b8f6598fd8d7
Can <cannedsolutions@gmail.com>
2020-04-24 17:50:44 +0200

    Updated settings.py

--- CannedSolutions/settings.py
+++ CannedSolutions/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x(8xqq=7bl%f#3xd!)bxh6a+_r&grw9p=wlka8t)aj3%0rj38%'
-
+# SECRET_KEY = 'x(8xqq=7bl%f#3xd!)bxh6a+_r&grw9p=wlka8t)aj3%0rj38%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
