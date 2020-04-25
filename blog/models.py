9e9006762477f70041ba6d77c9bf36a0186195eb
Can <cannedsolutions@gmail.com>
2020-04-25 14:17:19 +0200

    add import PIL in blog/models

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from PIL import Image
 
 class Post(models.Model):
 	title = models.CharField(max_length = 100)
 	content = models.TextField()
 	date_posted = models.DateTimeField(default=timezone.now)
-	image = models.ImageField(default='default2.jpg', upload_to='profile_pics')
+	image = models.ImageField(default='post_default.jpg', upload_to='post_pics')
 
 	#if user is deleted, post will also be deleted
 	author = models.ForeignKey(User, on_delete = models.CASCADE) 
