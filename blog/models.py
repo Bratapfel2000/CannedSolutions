93a30b2b51ab13d1e8b6be205eb2e5530bdf42fb
Can <cannedsolutions@gmail.com>
2020-04-25 14:14:11 +0200

    Post images

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
 	title = models.CharField(max_length = 100)
 	content = models.TextField()
 	date_posted = models.DateTimeField(default=timezone.now)
+	image = models.ImageField(default='default2.jpg', upload_to='profile_pics')
 
 	#if user is deleted, post will also be deleted
 	author = models.ForeignKey(User, on_delete = models.CASCADE) 
